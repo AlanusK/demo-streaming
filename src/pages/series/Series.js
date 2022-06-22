8e5083fc33378997f247c167b14c07b22f1272c5
AlanusK <akajara@gmail.com>
2022-06-22 08:56:04 +0300

    feat: fetch json data

--- src/pages/series/Series.js
+++ src/pages/series/Series.js
@@ -1,4 +1,6 @@
+/* eslint-disable array-callback-return */
 import React from 'react';
+import Data from '../../data.json'
 
 const Series = () => {
 
@@ -8,7 +10,20 @@ const Series = () => {
                 <h1 className='text-2xl text-white font-medium'>Popular Series</h1>
             </div>
             <div className='px-20 pt-20 flex flex-wrap justify-start'>
-                Series Page
+                {/* Before maping, check if Data exists */}
+                {Data && Data.map((post, index) => {       
+                    if( post.releaseYear >= 2010 && post.programType === 'series')
+                    return (
+                        <button key={index} className='mr-10 my-3 flex flex-col w-32'>
+                            <div>
+                                <img src={post.images['Poster Art'].url} alt='' />
+                            </div>
+                            <div>
+                                <p className='mb-3 font-light w-32 text-left'>{post.title}</p>
+                            </div>
+                        </button>
+                    )
+                })}
             </div>
         </div>
     )
